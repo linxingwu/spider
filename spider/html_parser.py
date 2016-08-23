d2701a2d75aab84a7f58af80c16e916d39f5038f
linxingwu <xingwu_lin@foxmail.com>
2016-08-23 23:55:31 +0800

    数据持久化保存，还需要重新设计数据库结构

--- spider/html_parser.py
+++ spider/html_parser.py
@@ -6,9 +6,14 @@
 import re
 import urllib
 
+import MySQLdb
 from bs4 import BeautifulSoup
 
+
 class Parser(object):
+    def __init__(self):
+        self.conn = MySQLdb.connect(host="127.0.0.1", user="root", passwd="admin", port=3306, db="zhihu", charset="utf8")
+
     def parse(self, new_url, html_doc):
         if new_url is None or html_doc is None:
             return None
@@ -42,11 +47,51 @@ class Parser(object):
         data["answer"] = data_answer
         question = soup.find("a", href=re.compile(r'/question/\d+')).get_text()
         data["question"] = question
+        cursor = self.conn.cursor()
+        try:
+            # TODO 優化數據庫結構，將鏈接中網站id部分作爲外鍵
+            dbquery = "select id from question where question = %s  "
+            cursor.execute(dbquery, question)
+            if cursor.rowcount == 1:
+                result = cursor.fetchone()
+                print result
+                quest_id = result[0]
+                sql = "insert into answer (author,url,answer,question) VALUE (%s,%s,%s,%s)"
+                cursor.execute(sql,(data["author"], data["url"], data["answer"], quest_id))
+                self.conn.commit()
+        except Exception as e:
+            print e
+            self.conn.rollback()
+        finally:
+            cursor.close()
         return data
 
     def parseurl_list(self, html_doc):
         linklist = []
         soup = BeautifulSoup(html_doc, "html.parser", from_encoding="utf-8")
+        question = soup.find_all("a",class_="question_link")
+        if question is not None and len(question) > 0:
+            cursor = self.conn.cursor()
+            try:
+                for q in question:
+                    quest_link = "https://www.zhihu.com"+q["href"]
+                    quest_title = q.get_text()
+                    # TODO 優化數據庫結構，將鏈接中id作爲外鍵
+                    dbquery = "select question from question where url = %s"
+                    cursor.execute(dbquery, quest_link)
+                    list = cursor.fetchall()
+                    if cursor.rowcount > 0 and list[0][0] == quest_title:
+                        print u"%s已經存在，不再插入此條記錄。" % quest_title
+                    else:
+                        sql = "insert into question (question,url) value(%s,%s)"
+                        cursor.execute(sql, (quest_title, quest_link))
+                self.conn.commit()
+            except Exception, e:
+                self.conn.rollback()
+                print e
+            finally:
+                cursor.close()
+
         links = soup.find_all('a', class_="toggle-expand", text="显示全部")
         if links is not None and len(links) > 0:
             for link in links:
